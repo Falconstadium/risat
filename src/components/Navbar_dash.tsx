f867762e4af2bb32097a7cebe531ed6f159a96dd
Yassir <atertay@gmail.com>
2024-08-27 11:01:43 +0100

    updating

--- src/components/Navbar_dash.tsx
+++ src/components/Navbar_dash.tsx
@@ -3,7 +3,7 @@ import { Link } from "@tanstack/react-router"
 const Navbar_dash = () => {
   return (
     <>
-      <header className="bg-indigo-700 text-white py-4 px-4 lg:px-3">
+      <header className="bg-indigo-700 text-white py-4 px-4 lg:py-3 lg:px-12">
       <div>
         <Link to="/" className="tracking-wide">risat</Link>
       </div>
--- src/routes/note.tsx
+++ src/routes/note.tsx
@@ -5,12 +5,12 @@ export const Route = createFileRoute('/note')({
   component: () => 
   <>
     <Navbar_Note />
-    <section className='dark:bg-black-500 dark:text-light'>
+    <section className='dark:bg-black-500 dark:text-light grid place-content-center'>
       <h1>Hello world!</h1>
     </section>
-    <button className='absolute bottom-4 left-8'>
-      <Link to='/dashbord' className='flex items-center gap-2 bg-black-500 dark:bg-light text-light dark:text-black-500 text-sm py-2 px-4 font-medium rounded-md transition-colors duration-300 ease-in-out hover:bg-gray-700 dark:hover:bg-gray-300'>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+    <button className='absolute bottom-4 left-2'>
+      <Link to='/dashbord' className='flex items-center gap-2 bg-black-500 dark:bg-light text-light dark:text-black-500 text-[12px] py-2 px-4 font-medium rounded-md transition-colors duration-300 ease-in-out hover:bg-gray-700 dark:hover:bg-gray-300'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
         <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
         </svg>
         Return to Dashbord
