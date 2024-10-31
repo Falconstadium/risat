bef7359cf8a4b7f0a5c4010d6fdb80ba86fb0155
Yassir <atertay@gmail.com>
2024-10-31 12:22:23 +0100

    add ui

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,26 +1,9 @@
-import { Link } from "@tanstack/react-router";
-import todoImg from "../assets/img/todo img.jpg";
-import noteImg from "../assets/img/note img.jpg";
-import { useTranslation } from "react-i18next";
+import { DotBackgroundDemo } from "./aceternity";
 
 const Home = () => {
-  const { t } = useTranslation("global");
-
   return (
     <main className="grid w-full place-content-center dark:bg-black-500">
-      <section className="container mx-auto grid place-content-center gap-3 px-4 pt-12 dark:text-light lg:place-items-center lg:gap-5">
-        <div className="grid place-content-center gap-2">
-          <h1 className="text-center text-3xl font-bold leading-normal lg:text-5xl lg:leading-normal">
-            {t("hero.title")}
-          </h1>
-          <p className="text-center text-xs font-medium lg:text-sm">
-            {t("hero.parag")}
-          </p>
-        </div>
-        <button className="rounded-md bg-blue-700 px-6 py-2 text-sm font-medium text-white transition-colors duration-300 ease-in-out hover:bg-blue-600">
-          <Link to="/dashboard">{t("hero.btn")}</Link>
-        </button>
-      </section>
+      <DotBackgroundDemo />
     </main>
   );
 };
