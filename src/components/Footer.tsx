72b472dec2e6c13b87bf2023ffb375bf9655e375
Yassir <atertay@gmail.com>
2025-04-06 13:14:06 +0100

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,11 +8,7 @@ const Footer = () => {
     <footer className="flex items-center justify-between gap-2 bg-[#30475e] px-6 py-3 lg:justify-around lg:px-0">
       <p className="text-xs font-medium capitalize tracking-wide text-light lg:text-sm">
         {t("footer.right")}
-        <span className="text-sm font-semibold lowercase lg:text-base">
-          {" "}
-          risat
-        </span>{" "}
-        &copy; {new Date().getFullYear()}
+        {""} &copy; {new Date().getFullYear()}
       </p>
       <FooterLang />
     </footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -53,7 +53,7 @@ export default {
           }),
           "bg-dot": (value) => ({
             backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`,
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="24" height="24" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`,
             )}")`,
           }),
         },
