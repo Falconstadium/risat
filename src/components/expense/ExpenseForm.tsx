30eb1fe552d57598c7d1a6f2690874da4ce8f0c6
Yassir <atertay@gmail.com>
2025-10-11 20:11:15 +0100

    refactor: Add a pop-up form

--- src/components/expense/ExpenseForm.tsx
+++ src/components/expense/ExpenseForm.tsx
@@ -7,6 +7,7 @@ interface props {
   amount: number;
   setDescription: Dispatch<SetStateAction<string>>;
   setAmount: Dispatch<SetStateAction<number>>;
+  showForm: () => void;
 }
 
 const ExpenseForm = ({
@@ -15,34 +16,54 @@ const ExpenseForm = ({
   setDescription,
   amount,
   setAmount,
+  showForm,
 }: props) => {
   const { t } = useTranslation("global");
 
   return (
-    <form onSubmit={submitForm} className="grid gap-2">
-      <input
-        type="text"
-        aria-label="Description"
-        placeholder={t("expense.description")}
-        value={description || ""}
-        onChange={(e) => setDescription(e.target.value)}
-        className="rounded-sm border border-black-500 bg-transparent px-2 py-1 text-sm font-medium outline-none dark:border-light dark:text-light"
-      />
-      <input
-        type="number"
-        aria-label="Amount"
-        value={amount || 0}
-        onChange={(e) => setAmount(Number(e.target.value))}
-        className="rounded-sm border border-black-500 bg-transparent px-2 py-1 text-sm font-medium outline-none dark:border-light dark:text-light"
-      />
+    <main className="absolute left-0 top-0 z-50 flex min-h-dvh w-full items-center justify-center px-8 backdrop-blur-sm">
       <button
-        type="submit"
-        aria-label="Add to Transaction"
-        className="rounded bg-blue-700 px-3 py-1 text-xs font-medium text-light transition-colors duration-300 ease-in-out hover:bg-blue-600 md:text-sm"
+        type="button"
+        className="absolute right-6 top-52 animate-fadeIn rounded-full bg-slate-900 p-1 transition duration-200 ease-in-out hover:bg-slate-800 lg:right-72 lg:p-2"
+        onClick={showForm}
       >
-        {t("expense.transaction")}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="size-6 text-white lg:size-7"
+        >
+          <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
+        </svg>
       </button>
-    </form>
+      <form
+        onSubmit={submitForm}
+        className="mx-auto grid w-full max-w-lg gap-2"
+      >
+        <input
+          type="text"
+          aria-label="Description"
+          placeholder={t("expense.description")}
+          value={description || ""}
+          onChange={(e) => setDescription(e.target.value)}
+          className="rounded-sm border border-black-500 bg-transparent px-2 py-1 text-sm font-medium outline-none dark:border-light dark:text-light"
+        />
+        <input
+          type="number"
+          aria-label="Amount"
+          value={amount || 0}
+          onChange={(e) => setAmount(Number(e.target.value))}
+          className="rounded-sm border border-black-500 bg-transparent px-2 py-1 text-sm font-medium outline-none dark:border-light dark:text-light"
+        />
+        <button
+          type="submit"
+          aria-label="Add to Transaction"
+          className="rounded bg-blue-700 px-3 py-1 text-xs font-medium text-light transition-colors duration-300 ease-in-out hover:bg-blue-600 md:text-sm"
+        >
+          {t("expense.transaction")}
+        </button>
+      </form>
+    </main>
   );
 };
 
--- src/components/expense/ExpenseList.tsx
+++ src/components/expense/ExpenseList.tsx
@@ -27,59 +27,40 @@ const ExpenseList = ({ transaction, deleteItem }: props) => {
           >
             <div className="flex w-full items-center justify-between rounded-md border border-black-500 px-2 py-1 text-sm dark:border-light dark:text-light">
               <p className="font-medium capitalize">{item.description}</p>
-              <p className="text-sm font-medium">{item.amount}</p>
+              <p className="text-sm font-medium">${item.amount}</p>
             </div>
-            <div className="flex items-center gap-2">
-              <button
-                type="button"
-                onClick={() => deleteItem(item.id)}
-                className="rounded-full bg-red-700 p-1 text-light transition-colors duration-300 ease-in-out hover:bg-red-600"
-                aria-label="Delete Transaction"
+
+            <button
+              type="button"
+              onClick={() => deleteItem(item.id)}
+              className="rounded-full bg-red-700 p-1 text-light transition-colors duration-300 ease-in-out hover:bg-red-600"
+              aria-label="Delete Transaction"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-x-icon lucide-x"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="lucide lucide-x-icon lucide-x"
-                >
-                  <path d="M18 6 6 18" />
-                  <path d="m6 6 12 12" />
-                </svg>
-              </button>
-              <button
-                type="button"
-                className="rounded-full bg-green-700 p-1 text-light transition-colors duration-300 ease-in-out hover:bg-green-600"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="lucide lucide-square-pen-icon lucide-square-pen"
-                >
-                  <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-                  <path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z" />
-                </svg>
-              </button>
-            </div>
+                <path d="M18 6 6 18" />
+                <path d="m6 6 12 12" />
+              </svg>
+            </button>
           </div>
         );
       })}
-      <div className="absolute bottom-4 left-0 w-full px-4 pt-2 text-center dark:border-t-neutral-200 dark:text-light">
-        <h5>Total Amounts:</h5>
-        <p className="text-sm">${calculateTotalPrice()}</p>
-      </div>
+      {transaction.length != 0 && (
+        <div className="absolute bottom-4 left-0 flex w-full items-center justify-center gap-1 px-4 pt-2 text-center dark:border-t-neutral-200 dark:text-light">
+          <h5>Total Price:</h5>
+          <p className="text-lg font-semibold">${calculateTotalPrice()}</p>
+        </div>
+      )}
     </section>
   );
 };
