b69495ffd69ef2628bf7b6e60468f142d85d7a4b
Yassir <atertay@gmail.com>
2026-05-08 11:49:30 +0100

    style: Update text size

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,10 +11,10 @@ const Home = () => {
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-zinc-950"></div>
         <section className="container z-20 mx-auto grid min-h-dvh animate-fadeIn place-content-center gap-6 px-8 lg:place-items-center">
           <div>
-            <h1 className="bg-gradient-to-br from-neutral-700 to-neutral-500 bg-clip-text text-center font-Fancy text-4xl font-bold uppercase text-transparent dark:from-neutral-400 dark:to-neutral-600 lg:text-6xl xl:text-7xl">
+            <h1 className="bg-gradient-to-br from-neutral-700 to-neutral-500 bg-clip-text text-center font-Fancy text-4xl font-bold uppercase text-transparent dark:from-neutral-400 dark:to-neutral-600 lg:text-5xl xl:text-6xl">
               {t("hero.title")}
             </h1>
-            <p className="z-30 text-center font-Fancy font-semibold text-neutral-700 dark:text-neutral-400 lg:text-lg xl:text-xl">
+            <p className="z-30 text-center font-Fancy font-medium text-neutral-700 dark:text-neutral-400 lg:text-lg xl:text-xl">
               {t("hero.parag")}
             </p>
           </div>
